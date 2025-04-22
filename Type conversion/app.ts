a9a4dfec37aef5c1ee23f0ae99ca1df25390b93d
Mustafaadeel1 <mustafaadeel989@gmail.com>
2025-04-22 17:38:27 +0500

    " Typescript & JavaScript code "

---
+++ Type conversion/app.ts
@@ -0,0 +1,27 @@
+// Type conversion is changing one data type into another  we will discuss here sting and number conversion and will discuss boolean conversion in next folder  
+// Type Coercion is when js change the typing for us in backend
+
+
+// 
+// let num1 : number = 20;
+// let num2 : string = "30";
+
+// // Number(num2) 
+
+// console.log(num1+Number(num2)) // number type
+// console.log(typeof num2)
+
+
+// type converstion
+
+// let num1 : number = 20;
+
+// console.log(num1) // number type
+// console.log(String(num1)) // string type
+// console.log(num1) // number type
+
+
+// + convert number into string 
+// - string number 
+
+console.log(20 + "30") 
---
+++ if else statement/code.ts
@@ -0,0 +1,19 @@
+// If else code stucture 
+
+if (5 > 3) {
+    console.log("5 is greater than 3");
+}
+else {
+    console.log("5 is not greater than 3");
+}
+
+
+
+if (5 > 3) {
+    console.log("5 is greater than 3");
+}else if (5 < 3) {
+    console.log("5 is less than 3");
+}
+else {
+    console.log("5 is equal to 3");
+}
