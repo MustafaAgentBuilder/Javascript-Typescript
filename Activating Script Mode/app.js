2e43f46721ea778a260cba4e563cab847f607516
Mustafaadeel1 <mustafaadeel989@gmail.com>
2025-04-25 22:55:05 +0500

    " First Package Create To work "

---
+++ Activating Script Mode/app.js
@@ -0,0 +1,21 @@
+// User strict can strict us to write better code
+// It helps us to avoid mistakes
+"use strict";
+// To create package.json file
+// Command to run: npm init -y
+// Create a file called tsconfig.json
+// Command to run: tsc --init
+console.log("Hi This is Mustafa!");
+
+let driverName = true;
+
+let userName = "Mustafa";
+let userAge = 18;
+let userEmail = "mustafaadeel989@gmail.com";
+if (driverName)console.log("Driver Name is Abbas");
+console.log(userName);
+console.log(userAge);
+console.log(userEmail);
+// To run the code
+// Command to run: node app.js
+// Command npm run start 
---
+++ Activating Script Mode/app.ts
@@ -0,0 +1,8 @@
+// First we run packages.jason file this file can run tsconfig than tscon file can run app.ts and he can create app.js and run This is all Structure 
+// to create package.json file
+// Command to run: npm init -y
+
+// Create a file called tsconfig.json
+// Command to run: tsc --init
+
+console.log("Hi This is Mustafa!");
